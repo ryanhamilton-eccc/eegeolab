1e046ebe9f637d4f0a641b0bd43d953cd89b4b7e
Ryan Hamilton <ryan.hamilton@ec.gc.ca>
2023-10-05 12:59:14 -0400

    added test

---
+++ tests/test_bmath.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `eegeolab` package."""
+
+
+import unittest
+
+from eegeolab import bmath
+
+
+class TestEegeolab(unittest.TestCase):
+    """Tests for `eegeolab` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
