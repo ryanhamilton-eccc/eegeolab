a93e5999e12c4c824527336f06b02e78d36e63eb
Ryan Hamilton <ryan.hamilton@ec.gc.ca>
2023-10-05 11:57:19 -0400

    added savi calc

--- eegeolab/bmath.py
+++ eegeolab/bmath.py
@@ -32,3 +32,32 @@ class NDVI:
             ee.Image: a Image that represents the NDVI values
         """
         return image.normalizedDifference([self.nir, self.red]).rename(self.name)
+
+
+class SAVI:
+    def __init__(self, nir: str, red: str, L: float = 0.5, name: str = None) -> None:
+        self.nir = nir
+        self.red = red
+        self.L = L
+        self.name = name
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name: str):
+        self._name = "SAVI" if name is None else name
+
+    def __call__(self, image: ee.Image) -> Any:
+        return image.addBands(self.compute(image=image))
+
+    def compute(self, image: ee.Image) -> ee.Image:
+        return image.expression(
+            "(1 + L) * (NIR - RED) / (NIR + RED + L)",
+            {
+                "NIR": image.select(self.nir),
+                "RED": image.select(self.red),
+                "L": self.L,
+            },
+        ).rename(self.name)
